9a901ab64f35d449791143d138c4849297b67f13
Jay <jccruz009@yahoo.com>
2024-07-17 13:36:12 +0800

    update install script

---
+++ .config/nvim/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Find way to ensure the install file is only activated FROM the user's dotfiles directory
+
+SOURCE_DIR="$HOME/dotfiles/.config/nvim"
+DEST_DIR="$HOME/.config/nvim"
+
+# Check if the current directory is the dotfiles directory
+if [[ "$(pwd)" != "$SOURCE_DIR" ]]; then
+  echo "This script must be run from the dotfiles/.config/nvim directory!"
+  exit 1
+fi
+
+echo "Installing neovim into $DEST_DIR"
+cp -r "$SOURCE_DIR" "$DEST_DIR"
+
+echo "Neovim has been installed!"
